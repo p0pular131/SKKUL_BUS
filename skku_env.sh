41c71af85184866b1c75ccdf4634e2e279177204
p0pular131 <popular131@g.skku.edu>
2025-05-19 20:14:48 +0900

    Add: SKKU env path

--- skku_env.sh
+++ skku_env.sh
@@ -8,17 +8,17 @@ cd carla_garage/carla
 conda activate garage_2
 # 경로설정
 export CARLA_ROOT=/home/heven/carla_garage/carla
-export WORK_DIR=/home/heven/carla_garage
+export WORK_DIR=/home/heven/SKKUL_BUS
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
 export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
 export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
 export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":${PYTHONPATH}
-export PYTHONPATH=$PYTHONPATH:/home/heven/carla_garage/scenario_runner
-export SAVE_PATH=/home/heven/carla_garage/results
+export PYTHONPATH=$PYTHONPATH:/home/heven/SKKUL_BUS/scenario_runner
+export SAVE_PATH=/home/heven/SKKUL_BUS/results
 export DEBUG_CHALLENGE=1
-cd carla_garage/leaderboard/leaderboard
+cd SKKUL_BUS/leaderboard/leaderboard
 # 모델 검증 실행
-python3 leaderboard_evaluator_local.py --agent-config /home/heven/carla_garage/pretrained_models/all_towns --agent /home/heven/carla_garage/team_code/sensor_agent_skku.py --routes /home/heven/carla_garage/leaderboard/data/debug.xml --checkpoint $SAVE_PATH/result.json
+python3 leaderboard_evaluator_local.py --agent-config /home/heven/SKKUL_BUS/pretrained_models/all_towns --agent /home/heven/SKKUL_BUS/team_code/sensor_agent_skku.py --routes /home/heven/SKKUL_BUS/leaderboard/data/debug.xml --checkpoint $SAVE_PATH/result.json
 
 
 # No graphic 
